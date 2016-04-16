bbed3a7f0d38cfc556f18356913dd1ab6030dd2e
Ayudh Sharma <ayudh_5@hotmail.com>
2016-04-16 12:11:31 +0800

    First commit

---
+++ www/js/app.js
@@ -0,0 +1,141 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'ngCordova'])
+
+.run(function($ionicPlatform, GoogleMaps) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+
+    GoogleMaps.init();
+  })
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+  .state('map', {
+    url: '/',
+    templateUrl: 'templates/map.html',
+    controller: 'MapCtrl'
+  });
+
+  $urlRouterProvider.otherwise("/");
+
+})
+
+.factory('Markers', function($http) {
+
+  var markers = [];
+
+  return {
+    getMarkers: function(){
+
+      return $http.get("http://localhost/ionic-maps/markers.php").then(function(response){
+          markers = response;
+          return markers;
+      });
+
+    }
+  }
+
+})
+
+.factory('GoogleMaps', function($cordovaGeolocation, Markers){
+
+  var apiKey = false;
+  var map = null;
+
+  function initMap(){
+
+    var options = {timeout: 10000, enableHighAccuracy: true};
+
+    $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+      
+      var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      var mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+      //Wait until the map is loaded
+      google.maps.event.addListenerOnce(map, 'idle', function(){
+
+        //Load the markers
+        loadMarkers();
+
+      });
+
+    }, function(error){
+      console.log("Could not get location");
+
+        //Load the markers
+        loadMarkers();
+    });
+
+  }
+
+  function loadMarkers(){
+
+      //Get all of the markers from our Markers factory
+      Markers.getMarkers().then(function(markers){
+
+        console.log("Markers: ", markers);
+
+        var records = markers.data.markers;
+
+        for (var i = 0; i < records.length; i++) {
+
+          var record = records[i];   
+          var markerPos = new google.maps.LatLng(record.lat, record.lng);
+
+          // Add the markerto the map
+          var marker = new google.maps.Marker({
+              map: map,
+              animation: google.maps.Animation.DROP,
+              position: markerPos
+          });
+
+          //var infoWindowContent = "<h4>" + record.name + "</h4>";
+		    var infoWindowContent = '<IMG BORDER="0" ALIGN="Left" SRC="http://localhost/ionic-maps/GmomsindaHoodz.jpg">'
+
+          addInfoWindow(marker, infoWindowContent, record);
+  
+        }
+
+      }); 
+
+  }
+
+  function addInfoWindow(marker, message, record) {
+
+      var infoWindow = new google.maps.InfoWindow({
+          content: message
+      });
+
+      google.maps.event.addListener(marker, 'click', function () {
+          infoWindow.open(map, marker);
+      });
+      
+  }
+
+  return {
+    init: function(){
+      initMap();
+    }
+  }
+
+})
